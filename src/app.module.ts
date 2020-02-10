87f321f5a85980606d9e417c9b6f8ff4ddefe333
Wesley Matos <wesley.matos@redfox.tech>
2020-02-10 12:28:49 -0300

    use typeorm on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      host: process.env.MYSQL_HOST,
+      type: 'mysql',
+      database: process.env.MYSQL_DATABASE,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      port: 3306,
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
