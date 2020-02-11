0dacdcb4d3d83033cce7f3097575defde145f7a6
Wesley Matos <wesley.matos@redfox.tech>
2020-02-11 16:32:15 -0300

    change docs path

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap(): Promise<void> {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(3000);
 }
