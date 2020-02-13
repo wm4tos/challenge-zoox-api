0b6011ec040fd90a6f4c2fe761d6ab10790e74d8
Wesley Matos <wesley.matos@redfox.tech>
2020-02-13 12:27:59 -0300

    write enum to state message responses

---
+++ src/modules/state/enums/messages.enum.ts
@@ -0,0 +1,6 @@
+export enum StateMessages {
+  NOT_FOUND_ERROR = 'Estado não encontrado.',
+  INEXISTENT_STATE = 'Estado inexistente.',
+  DUPLICATED = 'Estado já cadastrado.',
+  DELETED = 'Estado removido com sucesso.',
+};
