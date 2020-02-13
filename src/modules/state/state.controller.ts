19ec49059eab3507e3686a7f880f3f8642316b77
Wesley Matos <wesley.matos@redfox.tech>
2020-02-13 16:38:22 -0300

    write route to get all states

--- src/modules/state/state.controller.ts
+++ src/modules/state/state.controller.ts
@@ -1,10 +1,35 @@
+import { Get, Query, UseGuards, HttpStatus, NotFoundException } from '@nestjs/common';
+
+import { ResponseDto } from 'src/common/interfaces/response.dto';
+import { ApiResponse } from 'src/common/helpers/api-response.helper';
 import { Controller } from 'src/common/helpers/controller.helper';
 
 import { StateService } from './state.service';
+import { State } from './state.entity';
+import { StateMessages } from './enums/messages.enum';
+import { AuthGuard } from '@nestjs/passport';
 
-Controller('states')
+@Controller('states')
 export class StateController {
   constructor(
     private readonly stateService: StateService,
   ) {}
+
+  @Get()
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Estados retornados com sucesso.',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Nenhum estado encontrado.',
+  })
+  @UseGuards(AuthGuard('jwt'))
+  async getAll(@Query() query?: State): Promise<ResponseDto> {
+    const states = await this.stateService.findAll(query);
+
+    if (states.length) return new ResponseDto(true, states);
+
+    throw new NotFoundException(new ResponseDto(false, states, StateMessages.NOT_FOUND_ERROR));
+  }
 }
