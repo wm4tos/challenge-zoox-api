3e75bb329a081dc8767c2faaeb801d0f12229a04
Wesley Matos <wesley.matos@redfox.tech>
2020-02-19 10:29:14 -0300

    write route to get one city

--- src/modules/cities/cities.controller.ts
+++ src/modules/cities/cities.controller.ts
@@ -1,12 +1,13 @@
-import { Get, HttpStatus, NotFoundException } from '@nestjs/common';
+import { Get, HttpStatus, NotFoundException, Param, Post, Body, ConflictException } from '@nestjs/common';
+import { ObjectId } from 'mongodb';
 
 import { Controller } from 'src/common/helpers/controller.helper';
 import { ApiResponse } from 'src/common/helpers/api-response.helper';
+import { ResponseDto } from 'src/common/interfaces/response.dto';
 
 import { CitiesService } from './cities.service';
 import { CityMessages } from './enums/messages.enum';
 import { CityDto } from './dtos/city.dto';
-import { ResponseDto } from 'src/common/interfaces/response.dto';
 
 @Controller('cities')
 export class CitiesController {
@@ -17,7 +18,7 @@ export class CitiesController {
   @Get()
   @ApiResponse({
     status: HttpStatus.OK,
-    description: 'Cidades retornadas com sucesso.',
+    description: CityMessages.OK,
   })
   @ApiResponse({
     status: HttpStatus.NOT_FOUND,
@@ -29,5 +30,22 @@ export class CitiesController {
     if (cities.length) return new ResponseDto(true, cities);
 
     throw new NotFoundException(new ResponseDto(false, cities, CityMessages.NOT_FOUND))
+  }
+
+  @Get('/:_id')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: CityMessages.OK,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: CityMessages.NOT_FOUND,
+  })
+  async getOne(@Param('_id') _id: ObjectId): Promise<ResponseDto> {
+    const city = await this.citiesService.findOne({ _id });
+
+    if (city) return new ResponseDto(true, city);
+
+    throw new NotFoundException(new ResponseDto(false, null, CityMessages.NOT_FOUND))
   }
 }
