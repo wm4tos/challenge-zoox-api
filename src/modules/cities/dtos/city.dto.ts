22c334f153411f79eb2b824994b6818f6f9cff9c
Wesley Matos <wesley.matos@redfox.tech>
2020-02-19 13:02:40 -0300

    use objectId instead StateDto

--- src/modules/cities/dtos/city.dto.ts
+++ src/modules/cities/dtos/city.dto.ts
@@ -1,13 +1,11 @@
 import { ObjectId } from 'mongodb';
 import { ApiProperty } from '@nestjs/swagger';
 
-import { StateDto } from 'src/modules/state/dtos/state.dto';
-
 export class CityDto {
   @ApiProperty()
   _id?: string | ObjectId;
   @ApiProperty()
   name?: string;
   @ApiProperty()
-  state?: StateDto;
+  state?: ObjectId;
 }
